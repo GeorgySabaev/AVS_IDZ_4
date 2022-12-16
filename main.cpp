981b2868272c6a5fd32c03dbb84732c7f4ccc30b
SaGeDeveloper <55732192+SaGeDeveloper@users.noreply.github.com>
2022-12-17 02:03:18 +0300

    input

--- main.cpp
+++ main.cpp
@@ -25,6 +25,9 @@ class Person {
   }
   void visit_gallery(){
     sem_wait(&gallery);
+    std::stringstream message1;
+    message1 << "Person " << name << " entered the gallery.\n";
+    print(message1.str());
     while(std::rand() % 10 != 0){
       int painting_number = std::rand() % 5;
       sem_wait(&paintings[painting_number]);
@@ -33,9 +36,9 @@ class Person {
       print(message.str());
       sem_post(&paintings[painting_number]);
     }
-    std::stringstream message;
-    message << "Person " << name << " left the gallery.\n";
-    print(message.str());
+    std::stringstream message2;
+    message2 << "Person " << name << " left the gallery.\n";
+    print(message2.str());
     sem_post(&gallery);
   }
 };
@@ -47,11 +50,13 @@ void* start_visit(void* arg) {
 }
 
 int main(){
+  int n;
+  std::cin >> n;
   for (auto & painting : paintings) {
     sem_init(&painting, 0, 10);
   }
   sem_init(&gallery, 0, 50);
-  for (int i = 0; i < 300; ++i) {
+  for (int i = 0; i < n; ++i) {
     Person *p = new Person();
     pthread_t *visit = nullptr;
     pthread_create(visit, nullptr, start_visit, p);
